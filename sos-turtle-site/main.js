8e4cd7bd84ed6992b31e450d56bb9a6149116391
kina107 <ngando107@gmail.com>
2021-04-25 10:33:07 +0700

    sos-turtle-website

---
+++ sos-turtle-site/main.js
@@ -0,0 +1,15 @@
+var swiper0 = new Swiper('.swiper0', {
+    direction: 'vertical',
+    pagination: {
+      el: '.swiper-pagination0',
+      clickable: true,
+    },
+  });
+
+  var swiper1 = new Swiper('.swiper1', {
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination1',
+      clickable: true,
+    },
+  });
---
+++ vertical-swiper/main.js
@@ -0,0 +1,19 @@
+// const swiper = new Swiper('.swiper-container', {
+//     // Optional parameters
+//     direction: 'vertical',
+//     loop: true,
+  
+//     // If we need pagination
+//     pagination: {
+//       el: '.swiper-pagination',
+//     },   
+    
+//   });
+
+  var swiper = new Swiper('.swiper-container', {
+    direction: 'vertical',
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
